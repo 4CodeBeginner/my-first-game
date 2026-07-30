96de65787a939fbeccc91a257f578456fdc91191
Nopal <141114024+4CodeBeginner@users.noreply.github.com>
2026-07-30 22:18:21 +0700

    gerak kanan kiri

--- new-game-project/scripts/player.gd
+++ new-game-project/scripts/player.gd
@@ -1,23 +1,22 @@
 extends CharacterBody2D
 
-@export var speed := 250.0
-@export var jump_velocity := -450.0
+@export var speed: float = 250.0
+@export var jump_velocity: float = -450.0
 
-func _physics_process(delta):
-	# Gravity
+func _physics_process(delta: float) -> void:
+	# Tambahkan gravitasi jika player tidak menyentuh lantai
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Gerak kiri dan kanan
-	var direction = Input.get_axis("ui_left", "ui_right")
+	# Input kiri dan kanan
+	var direction := Input.get_axis("ui_left", "ui_right")
 
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+	# Gerakan horizontal
+	velocity.x = direction * speed
 
 	# Lompat
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = jump_velocity
 
+	# Jalankan pergerakan
 	move_and_slide()
